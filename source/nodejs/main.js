865ab647a9dc30a8880b6f941e5e2685ac76dabf
naseif <d2mds21@gmail.com>
2021-05-02 17:29:22 +0000

    Databse connection is now established. Configuration has its own file.

--- source/nodejs/main.js
+++ source/nodejs/main.js
@@ -1,12 +1,21 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const config = require("./config.json")
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword"
+let con = mysql.createConnection({
+    host: config.host,
+    user: config.username,
+    password: config.password,
+    database: config.database
   });
   
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    let sql = "SELECT ID, NAME FROM Board";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+       for (const {ID: ID, NAME: NAME}  of result){
+           console.log(ID, NAME)
+       }  
+      })
   });
