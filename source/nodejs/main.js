ffb5322fcd4aa0b21767dfdf93c1b70ce198145b
naseif <d2mds21@gmail.com>
2021-05-02 17:39:46 +0000

    Express.js webserver implemented

--- source/nodejs/main.js
+++ source/nodejs/main.js
@@ -1,21 +1,31 @@
 const mysql = require('mysql');
 const config = require("./config.json")
+const express = require('express')
+let app = express()
+const port = 51337;
 
-let con = mysql.createConnection({
-    host: config.host,
-    user: config.username,
-    password: config.password,
-    database: config.database
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    let sql = "SELECT ID, NAME FROM Board";
-    con.query(sql, function (err, result) {
+app.get('/', function (req, res) {
+    let con = mysql.createConnection({
+        host: config.host,
+        user: config.username,
+        password: config.password,
+        database: config.database
+      });
+      
+      con.connect(function(err) {
         if (err) throw err;
-       for (const {ID: ID, NAME: NAME}  of result){
-           console.log(ID, NAME)
-       }  
-      })
-  });
+        console.log("Connected!");
+        let sql = "SELECT ID, NAME FROM Board";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            res.send(result);
+            con.end();
+          })
+      });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+
+
