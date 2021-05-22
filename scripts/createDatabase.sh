2acf52ff8154ddcc1bab4785d43a002075fd97f0
naseif <d2mds21@gmail.com>
2021-05-22 18:06:55 +0000

    Added Shell script to create mysql database

---
+++ scripts/createDatabase.sh
@@ -0,0 +1,15 @@
+sudo apt install mysql-server
+sudo mysql -e 'CREATE DATABASE Axolotl;'
+cd .. 
+cd source/sql
+
+for i in *.sql
+do
+    echo "Importing: $i"
+    sudo mysql Axolotl < $i
+    wait
+done 
+
+sudo mysql -e "use Axolotl; CREATE USER 'Naseif'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Axolotl';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON Axolotl.* TO 'Naseif'@'localhost' WITH GRANT OPTION;"
+sudo mysql -e "use Axolotl; FLUSH PRIVILEGES;"
--- source/sql/1000-boardTable.sql
+++ source/sql/1000-boardTable.sql
@@ -1,4 +1,9 @@
 CREATE TABLE Board (
 	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`Name` VARCHAR(200) NOT NULL
-)
+);
+INSERT INTO Board (
+	`Name` 
+) Values (
+	'Naseif'
+);
